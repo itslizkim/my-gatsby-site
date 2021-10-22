c016dd4618c88acd93295bcaebbc8e0a74d8f5eb
itslizkim <elizabeth.kim93@gmail.com>
2021-10-22 14:54:26 -0400

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react";
-import Layout from '../components/layout'
+import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p> I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage 
+        alt="ChouChou, a white holland lop bunny staring at the camera"
+        src="../images/IMG_8422.jpg"
+      />
     </Layout>
   )
 }
